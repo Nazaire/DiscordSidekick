13a4dbe623bffc57ad24919fa5d481b056354ab5
Unknown <nathanrobb@iinet.net.au>
2018-05-08 13:24:28 +0800

    Dice
    Added support for 0 and null values, limited rolls to 30

--- src/methods/dice/dice.js
+++ src/methods/dice/dice.js
@@ -1,10 +1,11 @@
 /*
-Fornite module for discord-sidekick
+Dice module for discord-sidekick
 Author Nathan Robb
 
 COMMANDS:
-	!dice dXX Y
+	!dice dXX [Y]
 		Calls the program to roll Y X-sided dice
+		Rolls 1 dice if no Y arg
 	!flip
 		flips a coin
 */
@@ -29,25 +30,36 @@ function onMessage(msg){
 		parts = msg.content.split(" ");
 		if(parts.length != 1){
 			parts[1] = parts[1].replace('d','');
-			for(i = 0; i < parts[2]; i++){
-				temp1 = "Dice ";
-				temp1 = temp1.concat(i + 1);
-				temp1 = temp1.concat(" rolled: ");
+			if(parts[2] < 30 && parts[2] > 0){
+				i = 0
+				do{
+					i++
+					temp1 = "Dice ";
+					temp1 = temp1.concat(i);
+					temp1 = temp1.concat(" rolled: ");
+					temp1 = temp1.concat(roll(Number(parts[1])));
+					output.push(temp1);
+				}while(i < parts[2])
+			}
+			else if(parts[2] == 0){
+				output[0] = "*throws nothing*";
+			}
+			else if(parts[2] == null){
+				temp1 = ("Rolled: ");
 				temp1 = temp1.concat(roll(Number(parts[1])));
 				output.push(temp1);
 			}
-		}
-
-		else{
-			//console.log("error 1");
-			msg.channel.send("Usage error");
-		}
+			else{
+				output[0] = "Please limit your rolls to less then 30";
+			}
 	}
 	else if(msg.content.startsWith("!flip")) output[0] = getFlip();
 
-	for(i = 0; i < output.length; i++){
-		//console.log(output[i]);
-		msg.channel.send(output[i]);
+	var ret = "";
+	for(i = 0; i < output.length; i++) ret = ret + output[i] +"\n";
+
+	//console.log(ret);
+	msg.channel.send(ret);
 	}
 }
 
